9e69fd2e4de3c81fd98ce198c5e49e5c66ed2eca
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2017-06-19 12:10:51 -0300

    Route for classrooms

--- src/actions/getClassrooms.js
+++ src/actions/getClassrooms.js
@@ -1,10 +1,21 @@
+const Student = require('../model/student');
+const Invite = require('../model/invite');
 const Classroom = require('../model/classroom');
 const { assertTeacher } = require('../utils');
 
 function getClassroom(user) {
     assertTeacher(user);
 
-    return Classroom.find({ teacher: user._id });
+    return Classroom.find({ teacher: user._id })
+        .lean()
+        .map(classroom => {
+            const query = { classroom: classroom._id };
+            return all([Student.find(query).lean(), Invite.find(query).lean()])
+                .spread((registered, invited) =>
+                    merge(classroom, {
+                        registered: pick(['email', 'name'], registered),
+                        invited: pick(['email', 'sent'], invited) }));
+        });
 }
 
 module.exports = getClassroom;
