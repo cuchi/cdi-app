5371a1003eb73a0690be2871feb247787a66a0a4
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2017-06-27 23:25:40 -0300

    Deleting invites when creating a student

--- src/actions/getRanking.js
+++ src/actions/getRanking.js
@@ -1,14 +1,24 @@
 const Student = require('../model/student');
-const { isTeacher } = require('../utils');
-const { assertStudent } = require('../utils');
+const Teacher = require('../model/teacher');
+const Classroom = require('../model/classroom');
 
-function getRanking(user) {
-    assertStudent(user);
-
+function getRankingForStudent(user) {
     return Student.aggregate([
         { $match: { classroom: user.classroom } },
         { $project: { name: true, code: true, score: true } },
         { $sort: { score: -1 } }]);
+}
+
+function getRankingForTeacher(user) {
+    return Teacher.findById(user._id)
+        .lean()
+        .then(teacher => //aaaa)
+}
+
+function getRanking(user) {
+    return user.model === 'Teacher'
+        ? getRankingForTeacher(user)
+        : getRankingForStudent(user);
 }
 
 module.exports = getRanking;
